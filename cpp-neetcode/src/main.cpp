32798bc8e9a8c458ba04d00f32e0354dd9f071c9
Alec MacMillen <alecmacmillen@gmail.com>
2026-08-06 16:55:59 -0600

    rework for valid parentheses problem

--- cpp-neetcode/src/main.cpp
+++ cpp-neetcode/src/main.cpp
@@ -1,49 +1,63 @@
 // Neetcode: longest_consecutive.cpp
 #include <iostream>
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
+#include <string>
 #include <cassert>
 
 using namespace std;
 
-int longest_consecutive(vector<int>& nums) {
-    // Create tracker variable and convert input vector to set
-    int longest_seq = 0;
-    std::unordered_set<int> numset(nums.begin(), nums.end());
+int valid_parentheses(std::string s) {
+    // Create hash map to link closing characters to opening
+    // Make sure to use single quotes ' to denote char, not string
+    std::unordered_map<char, char> closure = {
+        {')', '('},
+        {']', '['},
+        {'}', '{'}
+    };
+    // Create empty stack to store the chars from input s
+    std::vector<char> stack = {};
 
-    // for loop: syntax iterates through objects in vector, not indices
-    for (const auto& item : numset) {
-        // Skip to next iteration if prev value is in the set; can't
-        // be the start of a sequence
-        if (numset.contains(item - 1)) {
-            continue;
+    // For loop to iterate through s
+    for (const auto& c : s) {
+        // If the character is a closing character
+        if (closure.contains(c)) {
+            // If the stack is not empty and the last char is 
+            // the correct match, pop from the end and continue
+            if (!stack.empty() and stack.back() == closure[c]) {
+                stack.pop_back();
+            }
+            // If the stack doesn't end with the correct character,
+            // we can immediately return false
+            else {
+                return false;
+            }
         }
-
-        // Increment sequence length variable for each sequential
-        // value that is still in the unordered set
-        int val = item;
-        int seq_length = 0;
-        while (numset.contains(val)) {
-            val++;
-            seq_length++;
-        }
-
-        // Update longest sequence length if current sequence is longer
-        if (seq_length > longest_seq) {
-            longest_seq = seq_length;
+        // If the character is an opening character, append it to the stack
+        else {
+            stack.push_back(c);
         }
     }
-    return longest_seq;
+    // If we get all the way through the input and
+    // we are not left with an empty stack, we have an
+    // unmatched opener. An empty ending stack means the input was
+    // valid, a non-empty ending stack means it is not
+    return stack.empty();
 }
 
 int main() {
-    vector<int> test1 = {2,20,4,10,3,4,5};
-    int test1_result = longest_consecutive(test1);
-    assert(test1_result == 4 && "Error: test 1 result should be 4.");
-    cout << "Test 1 passed successfully!";
+    std::string test1 = "[]";
+    bool test1_result = valid_parentheses(test1);
+    assert(test1_result == true && "Error: test 1 result should be true.");
+    cout << "Test 1 passed successfully!\n";
 
-    vector<int> test2 = {0,3,2,5,4,6,1,1};
-    int test2_result = longest_consecutive(test2);
-    assert(test2_result == 7 && "Error: test 2 result should be 7.");
-    cout << "Test 2 passed successfully!";
+    std::string test2 = "([{}])";
+    bool test2_result = valid_parentheses(test2);
+    assert(test2_result == true && "Error: test 2 result should be true.");
+    cout << "Test 2 passed successfully!\n";
+
+    std::string test3 = "[(])";
+    bool test3_result = valid_parentheses(test3);
+    assert(test3_result == false && "Error: test 3 result should be false.");
+    cout << "Test 3 passed successfully!\n";
 }
